8496475f919d837aaa1a4f4e6e2c2575409fd760
FAUSSURIER Marc <faussurier.marc@icloud.com>
2019-03-13 10:31:42 +0100

    missing use statement

--- configs/db.sql
+++ configs/db.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE `cde_code` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+use `cde_code`;
 CREATE TABLE `groups` (
-  `row_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`row_id`)
+                        `row_id` int(11) NOT NULL AUTO_INCREMENT,
+                        `name` varchar(45) DEFAULT NULL,
+                        PRIMARY KEY (`row_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `users` (
-  `row_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(16) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(32) NOT NULL,
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`row_id`)
+                       `row_id` int(11) NOT NULL AUTO_INCREMENT,
+                       `username` varchar(16) NOT NULL,
+                       `email` varchar(255) DEFAULT NULL,
+                       `password` varchar(32) NOT NULL,
+                       `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                       `first_name` varchar(45) DEFAULT NULL,
+                       `last_name` varchar(45) DEFAULT NULL,
+                       PRIMARY KEY (`row_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
